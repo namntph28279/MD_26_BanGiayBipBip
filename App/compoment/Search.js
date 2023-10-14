70acec5f77cd13129535b046c74326df42085008
RinXe <giacatvu147@gmail.com>
2023-10-14 16:13:05 +0700

    Add search button

--- App/compoment/Search.js
+++ App/compoment/Search.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, ScrollView, Text, Image, TextInput, FlatList , TouchableOpacity, } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Image, TextInput, FlatList, TouchableOpacity } from "react-native";
 
 function Search({ navigation }) {
   const [products, setProducts] = useState([]);
-  const [searchText, setSearchText] = useState(""); // State để lưu giá trị của thanh tìm kiếm
+  const [searchText, setSearchText] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     fetch("https://md26bipbip-496b6598561d.herokuapp.com/")
@@ -18,41 +20,53 @@ function Search({ navigation }) {
 
   const renderProductItem = ({ item }) => {
     return (
-     
-        <TouchableOpacity style={styles.productContainer}
-          key={item._id}
-          onPress={() => {
-            navigation.navigate("ProductDetail", { productId: item._id });
-          }}>
-          <Image source={{ uri: item.product_image }} style={styles.productImage} />
-          <View style={styles.productDetails}>
-            <Text style={styles.productTitle}>{item.product_title}</Text>
-            <Text style={styles.productPrice}>Giá: {item.product_price}</Text>
-          </View>
-        </TouchableOpacity>
-      
+      <TouchableOpacity
+        style={styles.productContainer}
+        key={item._id}
+        onPress={() => {
+          navigation.navigate("ProductDetail", { productId: item._id });
+        }}>
+        <Image source={{ uri: item.product_image }} style={styles.productImage} />
+        <View style={styles.productDetails}>
+          <Text style={styles.productTitle}>{item.product_title}</Text>
+          <Text style={styles.productPrice}>Giá: {item.product_price}</Text>
+        </View>
+      </TouchableOpacity>
     );
   };
 
-  const filteredProducts = products.filter((item) =>
-    item.product_title.toLowerCase().includes(searchText.toLowerCase())
-  );
+  const handleSearch = () => {
+    setFilteredProducts(products.filter((item) =>
+      item.product_title.toLowerCase().includes(searchText.toLowerCase()))
+    );
+    setIsSearching(true);
+  };
 
   return (
-    <View style={styles.container}>
-      {/* Thanh tìm kiếm */}
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Tìm kiếm sản phẩm..."
-        value={searchText}
-        onChangeText={(text) => setSearchText(text)}
-      />
-
-      <FlatList
-        data={filteredProducts} // Sử dụng filteredProducts thay vì products
-        keyExtractor={(item) => item._id}
-        renderItem={renderProductItem}
-      />
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Tìm kiếm sản phẩm..."
+            value={searchText}
+            onChangeText={(text) => setSearchText(text)}
+          />
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={handleSearch}
+          >
+            <View style={styles.searchButtonSquare}>
+              <Text style={styles.searchButtonText}>Tìm</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={isSearching ? filteredProducts : products}
+          keyExtractor={(item) => item._id}
+          renderItem={renderProductItem}
+        />
+      </View>
     </View>
   );
 }
@@ -62,20 +76,44 @@ export default Search;
 const styles = StyleSheet.create({
   container: {
     paddingTop: 15,
-    backgroundColor: "#DDD",
-    marginBottom: 70
+    backgroundColor: "#dddddd",
+    marginBottom: "15%"
   },
+  searchContainer: {
+    flexDirection: "row",
+  },
+  
+  mainContainer: {
+    height: "100%",
+    backgroundColor: "#dddddd",
+  },
+ 
   searchInput: {
     margin: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: "#999",
     borderRadius: 10,
+    width: "80%"
+  },
+  searchButton: {
+    marginTop:14,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 40,
+    height: 40,
+    backgroundColor: "blue",
+    borderRadius: 5,
+    
+  },
+ 
+  searchButtonText: {
+    color: "white",
   },
   productContainer: {
     width: "90%",
     margin: 10,
-    marginLeft: "5%", // Canh giữa sản phẩm theo chiều ngang
+    marginLeft: "5%",
     padding: 10,
     backgroundColor: "white",
     elevation: 5,
@@ -83,8 +121,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 5,
-    flexDirection: "row", // Sử dụng flexbox để căn giữa theo chiều ngang
-    alignItems: "center", // Căn giữa theo chiều dọc
+    flexDirection: "row",
+    alignItems: "center",
     borderRadius: 10,
   },
   productImage: {
@@ -93,7 +131,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   productDetails: {
-    width: "70%", // Đặt chiều rộng của productDetails là 50% để nó nằm bên phải
+    width: "70%",
     padding: 10,
   },
   productTitle: {
