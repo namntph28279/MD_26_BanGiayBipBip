6cf2ec06402ecfb1047d8dfdd97481298633524e
NguyenHuuThiem <nguyenhuuthiem03112003@gmail.com>
2023-12-02 16:38:42 +0700

    .

--- App/screens/screenExtra/OrderMain.js
+++ App/screens/screenExtra/OrderMain.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import {View, useWindowDimensions, Text, FlatList, TouchableOpacity, Image, TextInput, StyleSheet} from 'react-native';
-import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import {fetchDataAndSetToRedux, fetchDataOrder} from "../../redux/AllData";
-import {getMonney} from "../../util/money";
+import { View, useWindowDimensions, Text, FlatList, TouchableOpacity, Image, TextInput, StyleSheet } from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchDataAndSetToRedux, fetchDataOrder } from "../../redux/AllData";
+import { getMonney } from "../../util/money";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import url from "../../api/url";
 
 
-export default function OrderMain({navigation}) {
+export default function OrderMain({ navigation }) {
     const layout = useWindowDimensions();
     const dataOrder = useSelector((state) => state.dataAll.dataDonHang);
     const dispatch = useDispatch();
@@ -21,35 +21,38 @@ export default function OrderMain({navigation}) {
     const [huyDonHang, setHuyDonHang] = useState([]);
     const [traDonHang, setTraDonHang] = useState([]);
     const [tatCaDon, setTatCaDon] = useState([]);
+    const [isSuccessModalVisible, setSuccessModalVisible] = useState(false);
+    const [isReturnReasonVisible, setReturnReasonVisible] = useState(false);
+    const [returnReason, setReturnReason] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
             dispatch(fetchDataOrder())
         };
-        const intervalId = setInterval(fetchData, 1100);
-        return () => clearInterval(intervalId);
+        // const intervalId = setInterval(fetchData, 3000);
+        // return () => clearInterval(intervalId);
     }, []);
 
-   useEffect(() => {
-            const filterchoXacNhanDon = dataOrder.filter(item => item.status === 0);
-            setChoXacNhan(filterchoXacNhanDon)
+    useEffect(() => {
+        const filterchoXacNhanDon = dataOrder.filter(item => item.status === 0);
+        setChoXacNhan(filterchoXacNhanDon)
 
-            const filterlayHang = dataOrder.filter(item => item.status === 1);
-            setlayHang(filterlayHang)
+        const filterlayHang = dataOrder.filter(item => item.status === 1);
+        setlayHang(filterlayHang)
 
-            const filterGiaoHang = dataOrder.filter(item => item.status === 2);
-            setGiaoHang(filterGiaoHang)
+        const filterGiaoHang = dataOrder.filter(item => item.status === 2);
+        setGiaoHang(filterGiaoHang)
 
-            const filterDaGiaoHang = dataOrder.filter(item => item.status === 3);
-            setDaGiaoHang(filterDaGiaoHang)
+        const filterDaGiaoHang = dataOrder.filter(item => item.status === 3);
+        setDaGiaoHang(filterDaGiaoHang)
 
-            const filterchoHuyDonHang = dataOrder.filter(item => item.status === 4 || item.status === 8);
-            setHuyDonHang(filterchoHuyDonHang)
+        const filterchoHuyDonHang = dataOrder.filter(item => item.status === 4 || item.status === 8);
+        setHuyDonHang(filterchoHuyDonHang)
 
-            const filterchoTraDonHang = dataOrder.filter(item => item.status === 5 || item.status === 6);
-            setTraDonHang(filterchoTraDonHang)
+        const filterchoTraDonHang = dataOrder.filter(item => item.status === 5 || item.status === 6);
+        setTraDonHang(filterchoTraDonHang)
 
-            setTatCaDon(dataOrder)
+        setTatCaDon(dataOrder)
 
 
     }, [dataOrder]);
@@ -57,56 +60,160 @@ export default function OrderMain({navigation}) {
 
     const [index, setIndex] = React.useState(0);
     const [routes] = React.useState([
-        {key: 'xacNhan', title: 'Chờ xác nhận'},
-        {key: 'layHang', title: 'Chờ lấy hàng'},
-        {key: 'giaoHang', title: 'Chờ giao hàng'},
-        {key: 'giao', title: 'Đã giao'},
-        {key: 'huy', title: 'Đã hủy'},
-        {key: 'tra', title: 'Trả hàng'},
-        {key: 'allSP', title: 'Tất cả đơn'},
+        { key: 'xacNhan', title: 'Chờ xác nhận' },
+        { key: 'layHang', title: 'Chờ lấy hàng' },
+        { key: 'giaoHang', title: 'Chờ giao hàng' },
+        { key: 'giao', title: 'Đã giao' },
+        { key: 'huy', title: 'Đã hủy' },
+        { key: 'tra', title: 'Trả hàng' },
+        { key: 'allSP', title: 'Tất cả đơn' },
     ]);
 
+
+    const handleCancelOrder = (item) => {
+        // Thực hiện logic hủy đơn hàng ở đây
+        console.log('Hủy đơn hàng:', item.id);
+    };
+    const handleReturnOrder = async (item) => {
+        try {
+            // Kiểm tra xem đơn hàng có ở tab "Đã giao" không
+            if (item.status === 3) { // 3 là mã trạng thái của "Đã giao", điều này có thể thay đổi tùy vào mã trạng thái của bạn
+                // Hiển thị TextInput khi ấn vào nút "Trả hàng"
+                setReturnReasonVisible(true);
+            } else {
+                // Nếu đơn hàng không ở tab "Đã giao", có thể hiển thị cảnh báo hoặc không làm gì cả
+                Alert.alert('Lưu ý', 'Bạn chỉ có thể trả hàng cho các đơn hàng ở tab "Đã giao".');
+            }
+        } catch (error) {
+            console.error('Lỗi', error);
+        }
+    };
+
+    const confirmReturnOrder = async (item) => {
+        try {
+            const orderId = item.id;
+
+            if (!returnReason) {
+                Alert.alert('Lưu ý', 'Vui lòng nhập lý do trả hàng.');
+                return;
+            }
+            const response = await url.post(`/order/return/${orderId}`, {
+                noiDung: returnReason,
+
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            console.log('returnReason:', returnReason);
+            console.log('Server Response:', response); // Log the response
+
+            if (response.status === 200) {
+                setCancelModalVisible(false);
+                setSuccessModalVisible(true);
+                showSuccessModal();
+                fetchDataList();
+                setReturnReasonVisible(false); // Ẩn TextInput sau khi xác nhận trả hàng
+                setReturnReason('');
+            } else {
+                console.error('Lỗi', response.statusText);
+            }
+        } catch (error) {
+            console.error('Lỗi', error);
+        }
+    };
+
     const renderItem = ({ item }) => (
-        <TouchableOpacity
-            style={styles.frame}
-            onPress={() => {
-                // console.log('Item:', item);
-                navigation.navigate('InformationLine', { productId: item.products[0].productId, orderId: item.id }); // Truyền product ID vào params
-            }}
-        >
-            <View style={styles.productBox}>
-                {item.products.map((product) => (
-                    <View key={product._id} style={styles.productItemContainer}>
-                        <Image source={{ uri: product.img_product }} style={styles.productImage} />
-                        <View style={styles.productInfo}>
-                            {/* <Text style={styles.productName}>{`Tên sản phẩm: ${product.productId}`}</Text> */}
-                            <Text style={styles.productName}>{`${product.name_Product}`}</Text>
-                            <Text>{`Màu: ${product.name_Color}`}</Text>
-                            <Text>{`Size: ${product.name_Size}`}</Text>
-                            <View style={styles.quantityAndPriceContainer}>
-                                <Text>{`SL: ${product.quantityProduct}`}</Text>
-                                <Text style={{ color: '#FF0000', fontWeight: 'bold' }}>{`Giá: ${getMonney(product.name_Price)}`}</Text>
+        <View>
+            {item.products.map((product) => {
+                const productId = product.product;
+                return (
+                    <TouchableOpacity
+                    key={product._id}
+                        style={styles.frame}
+                        onPress={() => {
+                            // console.log('Item:', productId);
+                            navigation.navigate('InformationLine',
+                                {
+                                    productId: product.product,
+                                    orderId: item.id,
+                                });
+                        }}
+                    >
+                        <View style={styles.productBox}>
+                            {item.products.map((product) => (
+
+                                <View key={product._id} style={styles.productItemContainer}>
+                                    <Image source={{ uri: product.img_product }} style={styles.productImage} />
+                                    <View style={styles.productInfo}>
+                                        <Text style={styles.productName}>{`ID sản phẩm: ${product.product}`}</Text>
+                                        <Text style={styles.productName}>{`${product.name_Product}`}</Text>
+                                        <Text>{`Màu: ${product.name_Color}`}</Text>
+                                        <Text>{`Size: ${product.name_Size}`}</Text>
+                                        <View style={styles.quantityAndPriceContainer}>
+                                            <Text>{`SL: ${product.quantityProduct}`}</Text>
+                                            <Text style={{ color: '#FF0000', fontWeight: 'bold' }}>{`Giá: ${getMonney(product.name_Price)}`}</Text>
+                                        </View>
+                                        {/* <View><Text style={styles.productItemContainer1}></Text></View> */}
+                                    </View>
+                                </View>
+                            ))}
+
+                            <View style={styles.orderStatusContainer}>
+
+                                <Text style={styles.orderStatus}>{`Tổng sản phẩm thành tiền: ${getMonney(item.total_amount)}`}</Text>
+                                <Text style={styles.orderStatus}>{`status ${(item.status)}`}</Text>
                             </View>
-                            {/* <View><Text style={styles.productItemContainer1}></Text></View> */}
+                            <View style={styles.buttonContainer}>
+                                {item.status === 0 || item.status === 1 ? (
+                                    <TouchableOpacity style={styles.cancelOrderButton} onPress={() => handleCancelOrder(item)}>
+                                        <Ionicons name="close-outline" size={20} color="white" />
+                                        <Text style={styles.cancelOrderButtonText}>Hủy mua</Text>
+                                    </TouchableOpacity>
+                                ) : null}
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                {item.status === 3 ? (
+                                    <TouchableOpacity style={styles.cancelOrderButton}
+                                        onPress={() => handleReturnOrder(item)}
+                                    >
+                                        <Ionicons name="close-outline" size={20} color="white" />
+                                        <Text style={styles.cancelOrderButtonText}>Trả hàng</Text>
+                                    </TouchableOpacity>
+                                ) : null}
+
+
+                                {isReturnReasonVisible && item.status === 3 && (
+                                    <View style={{ padding: 10 }}>
+                                        <TextInput
+                                            placeholder="Nhập lý do trả hàng"
+                                            value={returnReason}
+                                            onChangeText={(text) => setReturnReason(text)}
+                                            style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5, padding: 8 }}
+                                        />
+                                        <TouchableOpacity onPress={() => confirmReturnOrder(item)}>
+                                            <Text style={styles.xacnhan}>Xác nhận trả hàng</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                )}
+                            </View>
+
+                            <View>
+                                {item.status === 5 ? (
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            navigation.navigate("ChatScreen");
+                                        }}
+                                    >
+                                        <Text style={styles.cancelOrderButtonText1}>Mọi thắc mắc về đơn hàng hãy liên hệ đến shop ngay !</Text>
+                                    </TouchableOpacity>
+                                ) : null}
+                            </View>
                         </View>
-                    </View>
-                ))}
-
-                <View style={styles.orderStatusContainer}>
-
-                    <Text style={styles.orderStatus}>{`Tổng sản phẩm thành tiền: ${getMonney(item.total_amount)}`}</Text>
-                    {/* <Text style={styles.orderStatus}>{`Tổng ${(item.status)}`}</Text> */}
-                </View>
-                <View style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.cancelOrderButton} onPress={() => handleCancelOrder(item)}>
-                            <Ionicons name="close-outline" size={20} color="white" />
-                            <Text style={styles.cancelOrderButtonText}>Hủy mua</Text>
-                        </TouchableOpacity>
-                </View>
-
-
-            </View>
-        </TouchableOpacity>
+                    </TouchableOpacity>
+                )
+            })}
+        </View>
     );
 
     const choXacNhan = () => (
@@ -122,27 +229,81 @@ export default function OrderMain({navigation}) {
     );
 
     const choLayHang = () => (
-        <Text>Chờ Lấy Hàng</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={cholayHangDon}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
 
     const choGiaoHang = () => (
-        <Text>Chờ Giao Hàng</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={chogiaoHang}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
-
     const daGiao = () => (
-        <Text>Đã giao</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={daGiaoHang}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
 
     const daHuy = () => (
-        <Text>Đã hủy</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={huyDonHang}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
 
     const traHang = () => (
-        <Text>Trả hàng</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={traDonHang}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
 
     const sanPhamDat = () => (
-        <Text>Sản phẩm đặt</Text>
+        <View>
+            <FlatList
+                style={styles.list}
+                data={tatCaDon}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item }) => {
+                    // console.log('Item:', item); // Add this line to log the item
+                    return renderItem({ item });
+                }}
+            />
+        </View>
     );
 
     const renderScene = SceneMap({
@@ -158,11 +319,11 @@ export default function OrderMain({navigation}) {
 
     return (
         <TabView
-            navigationState={{index, routes}}
+            navigationState={{ index, routes }}
             swipeEnabled={false}
             renderScene={renderScene}
             onIndexChange={setIndex}
-            initialLayout={{width: layout.width}}
+            initialLayout={{ width: layout.width }}
             activeTintColor="black"
             inactiveTintColor="black"
             renderTabBar={props => (
@@ -171,8 +332,8 @@ export default function OrderMain({navigation}) {
                     scrollEnabled
                     activeColor="black"
                     inactiveColor="black"
-                    indicatorStyle={{backgroundColor: 'black'}}
-                    style={{backgroundColor: '#f2f4f8',}}
+                    indicatorStyle={{ backgroundColor: 'black' }}
+                    style={{ backgroundColor: '#f2f4f8', }}
                 />
             )}
         />
