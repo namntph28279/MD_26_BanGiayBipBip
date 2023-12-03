e5a7287c8b76f1a194a21154d0c6e337c9e4f505
RinXe <giacatvu147@gmail.com>
2023-12-03 21:59:59 +0700

    luồng

--- App/screens/screenMain/Search.js
+++ App/screens/screenMain/Search.js
@@ -315,7 +315,7 @@ const styles = StyleSheet.create({
   saleImageContainer: {
     width: "100%",
     height: 160,
-    paddingBottom: 3,
+    paddingBottom: 10,
     borderRadius: 10,
   },
   saleProductImage: {
